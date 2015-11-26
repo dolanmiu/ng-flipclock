d44fe15a3278a8b40507309cb6053bd090e28be5
Dolan Miu <dolan_miu@hotmail.com>
2015-11-26 22:34:56 +0000

    added more options to align with FlipClock API

--- ng-flipclock.js
+++ ng-flipclock.js
@@ -4,48 +4,73 @@ angular.module('ng-flipclock', []).directive('flipClock', ['$parse', function ($
 
     return {
         restrict: 'E',
-        //replace: true,
         scope: {
-            autostart: '@',
+            autoStart: '@',
             countdown: '@',
             callbacks: '@',
             classes: '@',
             clockFace: '@',
-            defaultclockface: '@',
-            defaultlanguage: '@',
-            time: '@',
-            clock: '='
+            defaultClockFace: '@',
+            defaultLanguage: '@',
+            countdownTo: '@',
+            clock: '=',
+            destroyCallback: '&',
+            createCallback: '&',
+            initCallback: '&',
+            intervalCallback: '&',
+            startCallback: '&',
+            stopCallback: '&',
+            resetCallback: '&'
         },
-        //template: '<div></div>',
-        link: function (scope, element, attr) {
-
-            var optionKeys = [
-              'autostart', //(boolean) If this is set to false, the clock will not auto start. The default value is true.
-              'countdown', //(boolean) If this is set to true, the clock will count down instead of up. The default value is false.
-              'callbacks', //(object) An object of callback functions that are triggered at various times. For more information, view the callback documentation.
-              'classes', //(object) This is an object of CSS classes to use to append to DOM objects
-              'clockFace', // (string) This is the name of the clock that is used to build the clock display. The default value is HourlyCounter.
-              'defaultclockface', //(string) This is the default clock face to use if the defined clock face does not exist. The default value is HourlyCounter.
-              'defaultlanguage' //(string) This is the default langauge to use. The default value is english.
-            ],
-                options = {
-                    callbacks: {}
-                },
-                methods = [
-            'start', //This method will start the clock just call the .start() method on an FlipClock object.
-            'stop', // This method will stop the clock just call the .stop() method on an FlipClock object.
-            'setTime', //This method will set the clock time after it has been instantiated just call the .setTime() method on an FlipClock object.
-            'getTime' //To get the clock time after it has been instantiated just call the .getTime() method on an FlipClock object.
-          ];
-
-            scope.clock = element.FlipClock(options);
-
-            /*//bind methods to the scope
-            methods.forEach(function (method) {
-                scope[method] = function () {
-                    clock[method].apply(clock, arguments);
+        controller: ['$scope', function ($scope) {
+            $scope.parse = function (value) {
+                if (value === undefined) {
+                    return undefined;
+                } else {
+                    if (value == 'true' || value == 'false') {
+                        return JSON.parse(value);
+                    } else {
+                        return value;
+                    }
                 }
-            });*/
+            };
+        }],
+        link: function (scope, element, attr, controller) {
+            var toDate,
+                currentDate = new Date(),
+                toDateSeconds;
+
+            if (scope.countdownTo instanceof Date) {
+                toDate = scope.countdownTo;
+            } else {
+                toDate = new Date(scope.countdownTo);
+            }
+            toDateSeconds = Math.floor((toDate - currentDate) / 1000);
+
+            scope.clock = element.FlipClock({
+                autoStart: scope.parse(scope.autoStart),
+                clockFace: scope.parse(scope.clockFace),
+                defaultClockFace: scope.parse(scope.defaultClockFace),
+                defaultLanguage: scope.parse(scope.defaultLanguage),
+                classes: scope.parse(scope.classes),
+                callbacks: {
+                    destroy: scope.destroyCallback,
+                    create: scope.createCallback,
+                    init: scope.initCallback,
+                    interval: scope.intervalCallback,
+                    start: scope.startCallback,
+                    stop: scope.stopCallback,
+                    reset: scope.resetCallback
+                }
+            });
+
+            scope.clock.setTime(toDateSeconds);
+            if (scope.countdown) {
+                scope.clock.setCountdown(true);
+            }
+            if (scope.autoStart === undefined || scope.autoStart === true) {
+                scope.clock.start();
+            }
         }
-    }
-  }]);
+    };
+}]);
